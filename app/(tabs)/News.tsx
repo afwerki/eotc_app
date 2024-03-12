feb92e06ee9e56b3179a8d6efbcd7b4de5ea5fe1
Afwerki Ghirmay <afwerkighirmay@Afwerkis-MacBook-Air.local>
2024-03-12 23:28:42 +0000

    News Page update

--- app/(tabs)/News.tsx
+++ app/(tabs)/News.tsx
@@ -1,116 +1,216 @@
-import { View, Text, ImageBackground, FlatList, StyleSheet,ScrollView } from 'react-native'
-import React from 'react'
+import React from "react";
+import {
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+} from "react-native";
 
-import { Link } from 'expo-router'
+import { Link } from "expo-router";
 
 const Page = () => {
-  const  data=[
-    {id:1, title:"Prayers", description: "In Selassie church", updatedDate:"2024-02-14", image:require('../../assets/images/praying_monk.jpeg'), },
+  const items = [
     {
-      id:2,
-      title:"Bible study",
-      description: "Description for card 2",
-      updatedDate:"2024-02-14",
-      image:require('../../assets/images/cross_in_room.jpeg'),
+      img: "https://plus.unsplash.com/premium_photo-1661281316103-9aef5ad47c50?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80",
+      title: "New Study Finds Link Between Exercise and Brain Function",
+      author: "Samantha Lee",
+      authorImg:
+        "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2.5&w=256&h=256&q=80",
+      tag: "health",
+      date: "Mar 24, 2023",
     },
     {
-      id:3,
-      title:"The nearset church",
-      description: "Description for card 1",
-      updatedDate:"2024-02-14",
-      image:require('../../assets/images/inside_church.jpeg'),
+      img: "https://images.unsplash.com/photo-1519558260268-cde7e03a0152?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80",
+      title: "Tech Giant Announces New Line of Smart Home Devices",
+      author: "John Smith",
+      authorImg:
+        "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2.5&w=256&h=256&q=80",
+      tag: "technology",
+      date: "Mar 23, 2023",
     },
     {
-      id:4,
-      title:"Mezmurs",
-      description: "Description for card 2",
-      updatedDate:"2024-02-14",
-      image:require('../../assets/images/catedral_outside.jpeg'),
+      img: "https://images.unsplash.com/photo-1605367177286-f3d4789c47a0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2342&q=80",
+      title: "City Council Approves Plan to Expand Public Transportation",
+      author: "Emily Chen",
+      authorImg:
+        "https://images.unsplash.com/photo-1515621061946-eff1c2a352bd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1389&q=80",
+      tag: "politics",
+      date: "Mar 22, 2023",
     },
     {
-      id:5,
-      title:"Mezmurs",
-      description: "Description for card 2",
-      updatedDate:"2024-02-14",
-      image:require('../../assets/images/catedral_outside.jpeg'),
+      img: "https://images.unsplash.com/photo-1565615833231-e8c91a38a012?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80",
+      title: "Researchers Discover Potential Treatment for Alzheimer's",
+      author: "Samantha Lee",
+      authorImg:
+        "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2.5&w=256&h=256&q=80",
+      tag: "health",
+      date: "Mar 21, 2023",
     },
     {
-      id:6,
-      title:"Mezmurs",
-      description: "Description for card 2",
-      updatedDate:"2024-02-14",
-      image:require('../../assets/images/catedral_outside.jpeg'),
+      img: "https://images.unsplash.com/photo-1593941707874-ef25b8b4a92b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2344&q=80",
+      title: "New Startup Aims to Revolutionize Electric Car Market",
+      author: "John Smith",
+      authorImg:
+        "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2.5&w=256&h=256&q=80",
+      tag: "technology",
+      date: "Mar 20, 2023",
     },
-    
+    {
+      img: "https://plus.unsplash.com/premium_photo-1663050986883-a5bdd99a7fa5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2362&q=80",
+      title: "Local Election Results Are In: Democrats Retain Majority",
+      author: "Emily Chen",
+      authorImg:
+        "https://images.unsplash.com/photo-1515621061946-eff1c2a352bd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1389&q=80",
+      tag: "politics",
+      date: "Mar 19, 2023",
+    },
+
+    {
+      img: "https://plus.unsplash.com/premium_photo-1663050986883-a5bdd99a7fa5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2362&q=80",
+      title: "Local Election Results Are In: Democrats Retain Majority",
+      author: "Emily Chen",
+      authorImg:
+        "https://images.unsplash.com/photo-1515621061946-eff1c2a352bd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1389&q=80",
+      tag: "politics",
+      date: "Mar 19, 2023",
+    },
+
+    {
+      img: "https://plus.unsplash.com/premium_photo-1663050986883-a5bdd99a7fa5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2362&q=80",
+      title: "Local Election Results Are In: Democrats Retain Majority",
+      author: "Emily Chen",
+      authorImg:
+        "https://images.unsplash.com/photo-1515621061946-eff1c2a352bd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1389&q=80",
+      tag: "politics",
+      date: "Mar 19, 2023",
+    },
   ];
 
-  const renderItem = ({ item }) => (
-    <View style={styles.card}>
-      <Text style={styles.title}>{item.title}</Text>
-      <ImageBackground source={item.image} style={styles.image}>
-        <Text style={styles.description}>{item.description}</Text>
-        <Text style={styles.updateDate}>Last Updated: {item.updateDate}</Text>
-      </ImageBackground>
-    </View>
-  );
-
   return (
-    <ImageBackground source={require('../../assets/images/paper.jpeg')} style={styles.background}>
-      <FlatList
-        data={data}
-        renderItem={renderItem}
-        keyExtractor={item => item.id}
-        vertical
-      />
-       <View>
-      <Link href={"/(modals)/login"}> Login</Link>
-      <Link href={"/(modals)/Register"}> Register</Link>
-      <Link href={"/(modals)/flier"}> flier</Link>
-      <Link href={"/(modals)/Upload_news"}>ዜና ይጫኑ</Link>
-      <Link href={"/(modals)/Upload_mezmur"}> መዝሙር ይጫኑ </Link>
-      <Link href={"/listing/1337"}> Upload mezmur</Link>
-    </View>
-      </ImageBackground>
-  )
-}
+    <SafeAreaView style={{ backgroundColor: "#fff" }}>
+      <ScrollView contentContainerStyle={styles.container}>
+        <Text style={styles.title}>News Feed</Text>
+        {items.map(({ img, title, author, authorImg, tag, date }, index) => {
+          return (
+            <TouchableOpacity
+              key={index}
+              onPress={() => {
+                //handle on press
+              }}
+            >
+              <View style={styles.card}>
+                <Image
+                  alt=""
+                  resizeMode="cover"
+                  source={{ uri: img }}
+                  style={styles.cardImg}
+                />
+                <View style={styles.cardBoday}>
+                  <Text style={styles.cardTag}>{tag}</Text>
+                  <Text style={styles.cardTitle}>{title}</Text>
+
+                  <View style={styles.cardRow}>
+                    <View style={styles.cardRowItem}>
+                      <Image
+                        alt=""
+                        source={{ uri: authorImg }}
+                        style={styles.cardRowItemImage}
+                      />
+                      <Text style={styles.cardRowItemText}>{author}</Text>
+                    </View>
+                    <Text style={styles.cardRowDivider}>.</Text>
+                    <View style={styles.cardRowItem}>
+                      <Text style={styles.cardRowItemText}>{date}</Text>
+                    </View>
+                  </View>
+                </View>
+              </View>
+            </TouchableOpacity>
+          );
+        })}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 /**
  https://withfra.me/components/list-card-sections
  */
-const styles=StyleSheet.create({
-  background: {
-    flex: 1,
-    resizeMode: 'cover',
-    justifyContent: 'center',
-  },
-  card: {
-    margin: 30,
-    borderRadius: 10,
-    overflow: 'hidden',
-    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
   },
   title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    paddingVertical: 10,
-    backgroundColor: '#f0f0f0',
+    fontSize: 32,
+    fontWeight: "700",
+    color: "#1d1d1d",
+    marginBottom: 12,
   },
-  image: {
-    height: 200,
-    justifyContent: 'flex-end',
+  card: {
+    flexDirection: "row",
+    alignItems: "stretch",
+    borderRadius: 12,
+    marginBottom: 16,
+    backgroundColor: "#fff",
   },
-  description: {
-    paddingHorizontal: 10,
-    paddingBottom: 5,
-    fontSize: 20,
-    color:  'red'
+  cardImg: {
+    width: 96,
+    height: 96,
+    borderRadius: 12,
   },
-  updateDate: {
-    paddingHorizontal: 10,
-    paddingBottom: 10,
+  cardBoday: {
+    flex: 1,
+    flexShrink: 1,
+    flexBasis: 0,
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    paddingHorizontal: 16,
+  },
+  cardTag: {
+    fontWeight: "500",
     fontSize: 12,
-    color: '#666',
+    color: "#939393",
+    marginBottom: 7,
+    textTransform: "capitalize",
   },
-
-})
-export default Page
+  cardTitle: {
+    fontWeight: "600",
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#000",
+    marginBottom: 8,
+  },
+  cardRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: -8,
+    marginBottom: "auto",
+  },
+  cardRowItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 6,
+    borderRightWidth: 1,
+    borderColor: "transparent",
+  },
+  cardRowItemImage: {
+    width: 22,
+    height: 22,
+    borderRadius: 9999,
+    marginRight: 6,
+  },
+  cardRowItemText: {
+    fontWeight: "400",
+    fontSize: 13,
+    color: "#939393",
+  },
+  cardRowDivider: {
+    fontSize: 17,
+    fontWeight: "bold",
+    color: "#939393",
+  },
+});
+export default Page;
